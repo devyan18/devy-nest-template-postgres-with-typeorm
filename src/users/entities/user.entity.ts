175f30fb08ed939d38e40286c692d61380212591
devyan18 <epache17@gmail.com>
2023-10-28 04:42:09 -0300

    User table created

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
 export class User {
-  constructor(
-    public readonly id: string,
-    public readonly email: string,
-    public readonly password: string,
-    public readonly username: string,
-  ) {}
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  username: string;
+
+  BeforeInsert() {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
 }
